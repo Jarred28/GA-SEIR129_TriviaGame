43ec154c78aedc7bae53ad81a6dc0c6794446bbc
jarredev3 <jarredev3@gmail.com>
2020-01-09 21:33:15 -0500

    AddedCSS&FinishedGame

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GA-SEIR129_TriviaGame
+
+Have you been paying attention to the rap scene of 2019? Well here is a game that will test your knowledge.
+
+I used Javascript with HTML and CSS for this Game. 
+
+I first started with my HTMl and i linked all my scripts for the game. I then moved on to creating all the questions/types. From there i created a button form for when the user is done with there trivia game. After that i moved on to my js and started with adding logic to each question i had wrote in my html. I stated what the correct answer was for e
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function check(){
+function check(event){
+	event.preventDefault();
 
 	var question1 = document.game.question1.value;
 	var question2 = document.game.question2.value;
@@ -10,25 +11,25 @@ function check(){
 	var correct = 0;
 
 
-	if (question1 == "charlotte") {
+	if (question1 === "charlotte") {
 		correct++;
 }
-	if (question2 == "Nipsey Hussle") {
+	if (question2 === "Nipsey Hussle") {
 		correct++;
 }	
-	if (question3 == "Post Malone") {
+	if (question3 === "Post Malone") {
 		correct++;
 }
-	if (question4 == "Dababy") {
+	if (question4 === "Dababy") {
 		correct++;
 }
-	if (question5 == "Yes") {
+	if (question5 === "Yes") {
 		correct++;
 }
-	if (question6 == "False") {
+	if (question6 === "False") {
 		correct++;
 }
-	if (question7 == "165,000") {
+	if (question7 === "165,000") {
 		correct++;
 }
 	
@@ -50,8 +51,8 @@ if (correct > 5) {
 }
 
 
-
-	document.getElementById("after_submit").style.visibility = "visible";
+	document.getElementById("game").classList.add('shrinked');
+	document.getElementById("after_submit").classList.add('show');
 
     document.getElementById("message").innerHTML = messages[logic];
 	document.getElementById("number_correct").innerHTML = "Thats " + correct + " correct.";
